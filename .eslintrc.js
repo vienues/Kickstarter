11909ee97987c126d3efe04497f49abd83b86a22
hwubyte <mjohnlynnc@gmail.com>
2023-05-22 05:30:53 -0400

    added: component

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     ecmaVersion: 2020,
     // Allows for the parsing of modern ECMAScript features
     sourceType: 'module',
-    // Allows for the use of imports
+
     ecmaFeatures: {
       jsx: true,
     },
